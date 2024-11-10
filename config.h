a07d48da09cd7cca669a155ea4b1446ccc09aa64
David <david19kerrigan@gmail.com>
2024-11-10 12:44:32 -0500

    auto commit

--- config.h
+++ config.h
@@ -36,16 +36,16 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static const Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|Mod1Mask,     XK_Return, focusonce,   { 0 } },
+	{ MODKEY|Mod1Mask,     XK_Return, spawn,       { 0 } },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	{ MODKEY|Mod1Mask,     XK_l,      rotate,      { .i = +1 } },
+	{ MODKEY|Mod1Mask,     XK_h,      rotate,      { .i = -1 } },
+	{ MODKEY|Mod1Mask,     XK_n,      movetab,     { .i = -1 } },
+	{ MODKEY|Mod1Mask,     XK_m,      movetab,     { .i = +1 } },
+	//{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	//{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
 	{ MODKEY,               XK_1,      move,        { .i = 0 } },
 	{ MODKEY,               XK_2,      move,        { .i = 1 } },
 	{ MODKEY,               XK_3,      move,        { .i = 2 } },
@@ -57,7 +57,7 @@ static const Key keys[] = {
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY|Mod1Mask,               XK_BackSpace,      killclient,  { 0 } },
 
 	//{ MODKEY,               XK_u,      focusurgent, { 0 } },
 	//{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
