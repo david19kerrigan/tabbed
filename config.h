f54d36ba006b767c77e18ce0d71ad74a291f2211
David Kerrigan <david19kerrigan@gmail.com>
2025-01-12 14:42:56 -0500

    update binds

--- config.h
+++ config.h
@@ -36,8 +36,8 @@ static Bool npisrelative  = True;
 #define MODKEY ControlMask
 static const Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|Mod1Mask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|Mod1Mask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|Mod1Mask,     XK_o, focusonce,   { 0 } },
+	{ MODKEY|Mod1Mask,     XK_o, spawn,       { 0 } },
 
 	{ MODKEY|Mod1Mask,     XK_l,      rotate,      { .i = +1 } },
 	{ MODKEY|Mod1Mask,     XK_h,      rotate,      { .i = -1 } },
@@ -57,7 +57,7 @@ static const Key keys[] = {
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY|Mod1Mask,               XK_BackSpace,      killclient,  { 0 } },
+	{ MODKEY|Mod1Mask,               XK_q,      killclient,  { 0 } },
 
 	//{ MODKEY,               XK_u,      focusurgent, { 0 } },
 	//{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
